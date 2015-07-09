a3880c42fab435a76b0ee21e5182fb122f7bd019
quxx <Thomas.Claudi@iem.thm.de>
2015-07-09 17:22:23 +0200

    checkOnline

--- www/js/isOnline.js
+++ www/js/isOnline.js
@@ -1,7 +1,7 @@
 function isOnline() {
 	var networkStatus = navigator.conenction.type;
 	
-	if (networkStatus == Connection.NONE || networkStatus == Connection.UNKNOWN) {
+	if (networkStatus == Connection.NONE) {
 		onConnectionError();
 		return false;
 	} else {
