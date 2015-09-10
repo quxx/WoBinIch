74e1683303965211a7329eb95f4e6971b982bbf3
SilverhawkPX45 <d.kessler@onlinehome.de>
2015-09-10 17:52:35 +0200

    data_handler0.1
    JSON based data handler

---
+++ www/scripts/data_handler.js
@@ -0,0 +1,34 @@
+/**
+ * 
+ * Erstellt aus Rohdaten ein Objekt nach JSON.
+ *
+ * @method formatJSON
+ *
+ * @param {Double} timestamp - Timestamp des Eintrags auf den Chatserver, dient als eindeutige ID
+ * @param {String} username - User-/Loginname für THM Chatserver
+ * @param {String} password - Passwort für THM Chatserver
+ * @param {String} imgURL - Endgültige URL für Bildnachricht auf THM Chatserver - Muss vorher generiert und vom Server abgeholt werden!
+ * @param {String} geodata - Geolocation Data
+ * @param {String} score - Aktueller Punktestand des Spielers
+ * @param {Boolean} open - Ist die Frage noch offen oder bereits beantwortet?
+ *
+ * @result {Object} formattedJSON - Javascript Object in JSON Formatierung, welches die obigen Parameter beinhaltet
+ *
+ */
+
+function formatJSON(timestamp, username, password, imgURL, geodata, score, open) {
+
+    var text, formattedJSON;
+    text = '{ "timestamp" : "' + timestamp + '"';
+    text += ', "username" : "' + username + '"';
+    text += ', "password" : "' + password + '"';
+    text += ', "imgURL" : "' + imgURL + '"';
+    text += ', "geodata" : "' + geodata + '"';
+    text += ', "score" : "' + score + '"';
+    text += ', "open" : "' + open + '" }';
+
+    formattedJSON = JSON.parse(text);
+    return formattedJSON;
+
+}
+
