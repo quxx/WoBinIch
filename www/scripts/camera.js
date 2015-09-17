7c2d32c65e112976668b840dad92a3e275ea2052
quxx <Thomas.Claudi@iem.thm.de>
2015-09-17 03:09:33 +0200

    Camera

--- www/scripts/camera.js
+++ www/scripts/camera.js
@@ -1,14 +1,33 @@
 function takePicture () {
     navigator.camera.getPicture(onSuccess, onFail, { 
         quality: 30,
-        sourceType : Camera.PictureSourceType.CAMERA,
-        destinationType: Camera.DestinationType.FILE_URI,
-        saveToPhotoAlbum: true,
-        encodingType: Camera.EncodingType.JPEG,
+        sourceType : navigator.camera.PictureSourceType.CAMERA,
+        destinationType: navigator.camera.DestinationType.FILE_URI,
+        //saveToPhotoAlbum: true,
+        //encodingType: navigator.camera.EncodingType.JPEG,
         correctOrientation: true});
 
     function onSuccess(imageURI) {
-        alert(imageURI);
+        var req = new AjaxRequest("http://thm-chat.appspot.com/oop/uploadURL", function(response){
+            console.log(response);
+            var image = document.getElementById('picture');
+            image.src = imageURI;
+            var xhr = new XMLHttpRequest();
+            imageString = imageURI.substr(7);
+            var data = "fromUser=thomas.claudi&fromPassword=8273&toUser=D.kessler&type=img&image=" + imageString;
+            console.log(data);
+                
+            xhr.open("POST", response, true);
+            xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    // do something with response
+                    console.log(xhr.responseText);
+                }
+            };
+            xhr.send(data);
+        });
+        req.send();      
     }     
     
     
