7e4d95add4d81e124fdfd5bdebdf233535665a4c
quxx <Thomas.Claudi@iem.thm.de>
2015-09-17 20:15:46 +0200

    Camera
    Upload works :tada:

--- www/scripts/camera.js
+++ www/scripts/camera.js
@@ -1,40 +1,61 @@
 function takePicture () {
     navigator.camera.getPicture(onSuccess, onFail, { 
         quality: 30,
-        sourceType : navigator.camera.PictureSourceType.CAMERA,
         destinationType: navigator.camera.DestinationType.FILE_URI,
+        sourceType: navigator.camera.PictureSourceType.CAMERA,
         //saveToPhotoAlbum: true,
         //encodingType: navigator.camera.EncodingType.JPEG,
-        correctOrientation: true});
+        correctOrientation: true
+        });
 
     function onSuccess(imageURI) {
         var req = new AjaxRequest("http://thm-chat.appspot.com/oop/uploadURL", function(response){
-            console.log(response);
-            var image = document.getElementById('picture');
-            image.src = imageURI;
-            var imageName = imageURI.substring(imageURI.length-17, imageURI.length);
-            var xhr = new XMLHttpRequest();
-            var data = '------WebKitFormBoundarylUBuXB648GYZSJC4\nContent-Disposition: form-data; name="fromUser"\n\nthomas.claudi\n------WebKitFormBoundarylUBuXB648GYZSJC4\nContent-Disposition: form-data; name="fromPassword"\n\n8273\n------WebKitFormBoundarylUBuXB648GYZSJC4\nContent-Disposition: form-data; name="toUser"\n\nthomas.claudi\n------WebKitFormBoundarylUBuXB648GYZSJC4\nContent-Disposition: form-data; name="type"\n\nimg\n------WebKitFormBoundarylUBuXB648GYZSJC4\nContent-Disposition: form-data; name="image"; filename=' + imageURI  + '\nContent-Type: image/jpeg\n\n\n------WebKitFormBoundarylUBuXB648GYZSJC4--'
-            console.log(data);
-                
-            xhr.open("POST", response, true);
-            xhr.setRequestHeader("Content-type", 'multipart/form-data; boundary="----WebKitFormBoundarylUBuXB648GYZSJC4"'); 
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState == 4 && xhr.status == 200) {
-                    // do something with response
-                    console.log(xhr.responseText);
-                }
-                else {
-                    console.log(xhr.responseText);
-                }
+            //console.log(response);
+            //var image = document.getElementById('picture');
+            //image.src = imageURI;
+            //var imageName = imageURI.substring(imageURI.length-17, imageURI.length);
+            //var imagePfad = imageURI.substring(7, imageURI.length);
+            var options = new FileUploadOptions();
+            options.fileKey= "image";
+            options.fileName= imageURI.substr(imageURI.lastIndexOf('/')+1);
+            options.mimeType= "image/jpeg";
+            options.headers = {
+                Connection: "close"
             };
-            xhr.send(data);
+            options.chunkedMode = false;
+
+            var params = new Object();
+                params.fromUser = "thomas.claudi";
+                params.fromPassword = "8273";
+                params.toUser = "thomas.claudi";
+                params.type = "img";
+
+            options.params = params;
+
+            var ft = new FileTransfer();
+            ft.upload(imageURI, response, win, fail, options);
+            
+            function win(r) {
+                console.log("Code = " + r.responseCode);
+                console.log("Response = " + r.response);
+                console.log("Sent = " + r.bytesSent);
+            }
+    
+            function fail(error) {
+                alert("An error has occurred: Code = " + error.code);
+                console.log("upload error source " + error.source);
+                console.log("upload error target " + error.target);
+            }    
         });
-        req.send();      
-    }     
+        req.send();   
+    };     
+}     
     
     
-    function onFail(message) {
-        console.log('Failed because: ' + message);
-    }
+function onFail(message) {
+    console.log('Failed because: ' + message);
 }
+
+
+
+
