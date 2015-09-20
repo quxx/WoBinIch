04a404cd0d80dae993f4e37ec75a4fb51586397d
quxx <Thomas.Claudi@iem.thm.de>
2015-09-20 18:15:00 +0200

    imageHandler

--- www/scripts/camera.js
+++ www/scripts/camera.js
@@ -1,48 +1,14 @@
-function takePicture () {
+﻿function takePicture () {
     navigator.camera.getPicture(onSuccess, onFail, { 
         quality: 30,
         destinationType: navigator.camera.DestinationType.FILE_URI,
         sourceType: navigator.camera.PictureSourceType.CAMERA,
-        //saveToPhotoAlbum: true,
+        saveToPhotoAlbum: true,
         correctOrientation: true
     });
 
     function onSuccess(imageURI) {
-        var req = new AjaxRequest("http://thm-chat.appspot.com/oop/uploadURL", function(response){
-
-            var options = new FileUploadOptions();
-                options.fileKey= "image";
-                options.fileName= imageURI.substr(imageURI.lastIndexOf('/')+1);
-                options.mimeType= "image/jpeg";
-                options.headers = {
-                    Connection: "close"
-                };
-                options.chunkedMode = false;
-
-            var params = new Object();
-            //sehe ich richtig? hier müsste man dann ultimativ username und password aus localstorage lesen?
-                params.fromUser = "thomas.claudi";
-                params.fromPassword = "8273";
-                params.toUser = "thomas.claudi";
-                params.type = "img";
-
-            options.params = params;
-
-            var ft = new FileTransfer();
-            ft.upload(imageURI, response, win, fail, options);
-            
-            //Function on Success
-            function win(r) {
-            }
-            
-            //Function on Fail
-            function fail(error) {
-                alert("An error has occurred: Code = " + error.code);
-                console.log("upload error source " + error.source);
-                console.log("upload error target " + error.target);
-            }    
-        });
-        req.send();   
+        uploadImage(imageURI, "thomas.claudi");
     };     
 }     
     
--- www/scripts/imageHandler.js
+++ www/scripts/imageHandler.js
@@ -1,26 +1,66 @@
+// This is a JavaScript file
+
+function uploadImage(imageURI, consignee) {    
+    var req = new AjaxRequest("http://thm-chat.appspot.com/oop/uploadURL", function(response){
+
+            var options = new FileUploadOptions();
+                options.fileKey= "image";
+                options.fileName= imageURI.substr(imageURI.lastIndexOf('/')+1);
+                options.mimeType= "image/jpeg";
+                options.headers = {
+                    Connection: "close"
+                };
+                options.chunkedMode = false;
+
+            var params = new Object();
+                params.fromUser = window.localStorage.getItem("loginname");
+                params.fromPassword = window.localStorage.getItem("password");
+                params.toUser = consignee;
+                params.type = "img";
+
+            options.params = params;
+
+            var ft = new FileTransfer();
+            ft.upload(imageURI, response, win, fail, options);
+            
+            //Function on Success
+            function win(r) {
+            }
+            
+            //Function on Fail
+            function fail(error) {
+                console.log("An error has occurred: Code = " + error.code);
+                console.log("upload error source " + error.source);
+                console.log("upload error target " + error.target);
+            }    
+        });
+        req.send();   
+        
+}
+
 /**
  * 
- * LÃ¤d ein Bild von einer Url und speichert dieses in einem neuerstellten/vorhandenen Ordner auf dem Smartphone.
+ * Läd ein Bild von einer Url und speichert dieses in einem neuerstellten/vorhandenen Ordner auf dem Smartphone.
  *
  * @method downloadFile
  *
  * @param {String} URL - Die URL des zu donwloadenen Bildes
  * @param {String} Folder_Name - Der Ordnername in dem das Bild gespeichert werden soll
- * @param {String} File_Name - Dateiname fÃ¼r das heruntergeladene Bild
+ * @param {String} File_Name - Dateiname für das heruntergeladene Bild
  *
  *
  */
 function downloadFile(URL, Folder_Name, File_Name) {
-//Parameter Ã¼berprÃ¼fen
+//Parameter überprüfen
 if (URL == null && Folder_Name == null && File_Name == null) {
     return;
 } else {
-    //ÃœberprÃ¼fen ob Internet vorhanden ist
+    //Überprüfen ob Internet vorhanden ist
     var networkState = navigator.connection.type;
     if (networkState == Connection.NONE) {
         return;
     } else {
-        //Ist dies erfÃ¼llt wird nÃ¤chste Funktion angestoÃŸen
+        //Ist dies erfüllt wird nächste Funktion angestoßen
         download(URL, Folder_Name, File_Name); 
     }
   }
@@ -36,7 +76,7 @@ function download(URL, Folder_Name, File_Name) {
         var rootdir = fileSystem.root;
         var fp = rootdir.toURL(); // Gesamter Ordnerpfad
     
-        fp = fp + "/" + Folder_Name + "/" + File_Name + "." + "jpg"; // Ordnerpfad und Dateiname fÃ¼r das speichern
+        fp = fp + "/" + Folder_Name + "/" + File_Name + "." + "jpg"; // Ordnerpfad und Dateiname für das speichern
         filetransfer(download_link, fp);
     }
 
