a4c6bcf1489591a93e22766244ac496e113ad24d
quxx <Thomas.Claudi@iem.thm.de>
2015-09-27 00:51:17 +0200

    questions

--- www/scripts/showQuestions.js
+++ www/scripts/showQuestions.js
@@ -1,3 +1,16 @@
 function showQuestions() {
+    var questions = window.localStorage.getItem("questions");
+    //alert(questions);
+    var QJson = JSON.parse(questions);
+    //alert(QJson[0].imgPath);
     
+    var i;
+    for (i=0; i<QJson.length; i++) {
+        var carousel = document.createElement("ons-carousel-item");
+        carousel.innerHTML += '<p class="picture-buttons"><ons-button class="btn-send" onclick="takePicture()">Beantwortren</ons-button></p><img src="' + QJson[i].imgPath + '" id="picture" height="100%" width="100%"></img>';
+        document.getElementById("carousel").appendChild(carousel);
+    
+    //document.getElementById("carousel").insertAdjacentHTML('beforeend', '<ons-carousel-item><div><img src="' + QJson[i].imgPath + '" id="picture" height="100%" width="100%"></img></div></ons-carousel-item>');
+    
+    }
 }
