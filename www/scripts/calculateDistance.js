fb728312cdb89d896523c148a9434515fd023651
quxx <Thomas.Claudi@iem.thm.de>
2015-09-27 19:09:15 +0200

    CalculateDistance

--- www/scripts/calculateDistance.js
+++ www/scripts/calculateDistance.js
@@ -1,20 +1,27 @@
-//Berechnung Distanz zwischen 2 Koordinaten
-//nun umgeschrieben, sodass sie Parameter frisst!
+﻿/**
+ * 
+ * Bekommt 2 Koordinaten Paar übergeben (einmal die vom Spieler eingegebene und die richtige Antwort aus der Bildfrage) hiermit ermittelt die Funktion die Entfernung ziwschen diesen beiden und gibt sie in Km zurück.
+ *
+ * @method distance
+ *
+ * @param {String} lat1 - Latitude Wert der vom Spieler eingegbene Position
+ * @param {String} lon1 - Longitude Wert der vom Spieler eingegbene Position
+ * @param {String} lat2 - Latitude der richtigen Antwort
+ * @param {String} lon2 - Longitude der richtigen Antwort
+ *
+ * @result {Number} dist - Entfernung ziwschen beiden Koordinaten (in KM)
+ *
+ */
 
-function distance(playerlat, playerlon, answerlat, answerlon) {
+function distance(lat1, lon1, lat2, lon2) {
     var lat1, lon1, lat2, lon2, dist;
 
-    //Zurecht schneiden beider Strings für Latitude und Longitude Werte
-    lat1 = playerlat;
-    lon1 = playerlon;
+    var R = 6371; // Radius of the earth in km
+    var dLat = (lat2 - lat1) * Math.PI / 180;  // deg2rad below
+    var dLon = (lon2 - lon1) * Math.PI / 180;
+    var a = 0.5 - Math.cos(dLat)/2 + Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) * (1 - Math.cos(dLon))/2;
 
-    lat2 = answerlat;
-    lon2 = answerlon;
-
-    //alert(playerAnswer + "\n" + lat1 + "\n" + lon1 + "\n" + answer + "\n" + lat2 + "\n" + lon2);
-
-    //Berechnung der Distanz zwischen beiden Koordinaten (dist in Km)
-    dist = 6378.388 * Math.acos(Math.sin(lat1) * Math.sin(lat2) + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon2 - lon1));
-
+    dist =  R * 2 * Math.asin(Math.sqrt(a));
     return dist;
+
 }
