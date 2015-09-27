1cfcb3cdf6dc8d39757310ecb2ce4045b8ceec47
quxx <Thomas.Claudi@iem.thm.de>
2015-09-27 16:43:48 +0200

    Questions

--- www/scripts/showQuestions.js
+++ www/scripts/showQuestions.js
@@ -1,17 +1,21 @@
 var questions = window.localStorage.getItem("questions");
 var QJson = JSON.parse(questions);
-document.addEventListener("pageinit", function (e) {
-    if (e.target.id == "my-page") {
+document.addEventListener("pageinit", function(e) {
+      if (e.target.id == "my-page") {
         var i;
-        for (i = 0; i < QJson.length; i++) {
-            var carousel = document.createElement("ons-carousel-item");
-            carousel.innerHTML += '<p class="picture-buttons"><ons-button class="btn-send" onclick="">Beantworten</ons-button></p><img src="' + QJson[i].imgPath + '" id="picture" height="100%" width="100%"></img>';
-            //aus QJson[i].imgPath wird voraussichtlich statt dessen getImage(QJson[i]), nur als Vorwarnung.
-            document.getElementById("carousel").appendChild(carousel);
+        for (i=0; i<QJson.length; i++) {
+        var carousel = document.createElement("ons-carousel-item");
+        var timestamp = QJson[i].timestamp;
+        carousel.innerHTML += '<p class="picture-buttons"><ons-button class="btn-send" onclick="answer()">Beantworten</ons-button></p><img src="' + QJson[i].imgPath + '" id="picture" height="100%" width="100%"></img>';
+        document.getElementById("carousel").appendChild(carousel);
         }
-    }
-}, false);
+      }
+    }, false);
 
 function refresh() {
-    window.location = "home.html";
+    window.location="home.html";
+}
+
+function answer() {
+    slidingMenu.setMainPage('map.html', {closeMenu: true})
 }
