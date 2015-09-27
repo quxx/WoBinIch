846e6c228b5b37f150bb625fcb806b017a8f1e25
quxx <Thomas.Claudi@iem.thm.de>
2015-09-27 16:12:24 +0200

    shoQuestions

--- www/scripts/showQuestions.js
+++ www/scripts/showQuestions.js
@@ -1,16 +1,17 @@
-function showQuestions() {
-    var questions = window.localStorage.getItem("questions");
-    //alert(questions);
-    var QJson = JSON.parse(questions);
-    //alert(QJson[0].imgPath);
-    
-    var i;
-    for (i=0; i<QJson.length; i++) {
+
+var questions = window.localStorage.getItem("questions");
+var QJson = JSON.parse(questions);
+document.addEventListener("pageinit", function(e) {
+      if (e.target.id == "my-page") {
+        var i;
+        for (i=0; i<QJson.length; i++) {
         var carousel = document.createElement("ons-carousel-item");
-        carousel.innerHTML += '<p class="picture-buttons"><ons-button class="btn-send" onclick="takePicture()">Beantwortren</ons-button></p><img src="' + QJson[i].imgPath + '" id="picture" height="100%" width="100%"></img>';
+        carousel.innerHTML += '<p class="picture-buttons"><ons-button class="btn-send" onclick="">Beantworten</ons-button></p><img src="' + QJson[i].imgPath + '" id="picture" height="100%" width="100%"></img>';
         document.getElementById("carousel").appendChild(carousel);
-    
-    //document.getElementById("carousel").insertAdjacentHTML('beforeend', '<ons-carousel-item><div><img src="' + QJson[i].imgPath + '" id="picture" height="100%" width="100%"></img></div></ons-carousel-item>');
-    
-    }
+        }
+      }
+    }, false);
+
+function refresh() {
+    window.location="home.html";
 }
