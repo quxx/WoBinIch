ab0f5a2ae79537fea0b6d0385adba4f265701396
SilverhawkPX45 <d.kessler@onlinehome.de>
2015-09-28 17:39:55 +0200

    trying deviceReady

--- www/scripts/showQuestions.js
+++ www/scripts/showQuestions.js
@@ -1,21 +1,28 @@
-var questions = [];
-questions = JSON.parse(window.localStorage.getItem("questions"));
-document.addEventListener("pageinit", function(e) {
-      if (e.target.id == "my-page") {
-        var i;
-        for (i=0; i<questions.length; i++) {
-            getImage(questions[i]);
-        }
-      }
-    }, false);
+document.addEventListener("deviceready", onDeviceReady, false);
 
-function refresh() {
-    window.location="home.html";
+function onDeviceReady() {
+    var questions = [];
+    questions = JSON.parse(window.localStorage.getItem("questions"));
+    document.addEventListener("onpageshow", function (e) {
+        if (e.target.id == "my-page") {
+            var i;
+            for (i = 0; i < questions.length; i++) {
+                getImage(questions[i]);
+            }
+        }
+    }, false);
 }
 
+function refresh() {
+    window.location = "home.html";
+}
+
+
 function answer(clickedElement) {
-    var theId = clickedElement.id; 
+    var theId = clickedElement.id;
     //alert(theId);
     window.sessionStorage.setItem('answerTimestamp', theId);
-    slidingMenu.setMainPage('map.html', {closeMenu: true});
+    slidingMenu.setMainPage('map.html', {
+        closeMenu: true
+    });
 }
