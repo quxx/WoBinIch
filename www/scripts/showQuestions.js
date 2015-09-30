189ff34a4a3aa9f85f91b0b16bb6361ec6c6dcf2
quxx <Thomas.Claudi@iem.thm.de>
2015-09-30 18:03:47 +0200

    doc showQuestion.js

--- www/scripts/showQuestions.js
+++ www/scripts/showQuestions.js
@@ -1,17 +1,22 @@
 var questions = [];
 questions = JSON.parse(window.localStorage.getItem("questions"));
+/**
+ * Es wird das QuestionArray aus dem Localstorage gelesen, nun wird anhand des Timestamps überprüft ob die Frage schon beantwortet worden ist, ist dies der Fall wird die nächste Frage überpfüft,
+ * ist dies nich der Fall wird das dazugehörige Bild aus der Bildnachricht geladen (über getImage() wird dieses dann auch angezeigt)
+ *
+ * @function 
+ */
 document.addEventListener("pageinit", function(e)
 {
-//    if (window.localStorage.getItem("answeredQ") == null)
-//    {
+    if (window.localStorage.getItem("answeredQ") == null)
+    {
         window.localStorage.setItem("answeredQ", "");
-//    }
+    }
     if (e.target.id == "my-page")
     {
         var answered = window.localStorage.getItem("answeredQ");
         var i = 0;
         var x = false;
-        //        alert(questions[i].timestamp)
         while (x == false)
         {
             if (answered.search(questions[i].timestamp) == -1 && questions[i].open != false)
@@ -27,11 +32,17 @@ document.addEventListener("pageinit", function(e)
     }
 }, false);
 
-
+/**
+ * Bekommt ein Element übergeben speichert die dazugehörige ID in den Localstorage, und wechselt auf die map.html seite.
+ * Die Id des objekets behinhaltet den Timestamp des jeweils angeklickten Bildes, dieser wird für die zuordnung von Bild und Bildfrage gebraucht
+ *
+ * @function answer
+ * 
+ * @param {element} clickedElement Das übergeben Elemnt aus der play.html 
+ */
 function answer(clickedElement)
 {
     var theId = clickedElement.id;
-    //alert(theId);
     window.sessionStorage.setItem('answerTimestamp', theId);
     slidingMenu.setMainPage('map.html',
     {
