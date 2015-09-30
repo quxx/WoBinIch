e248f4d5764593074ef056ff1496544007d8e289
quxx <Thomas.Claudi@iem.thm.de>
2015-09-30 16:16:01 +0200

    geo.js

--- www/scripts/geo.js
+++ www/scripts/geo.js
@@ -1,14 +1,19 @@
+/**
+ *
+ * Holt sich die aktuellen Geodaten in Form von Altidue und Longitude und speichert sie in den Localstorage
+ *
+ * @method geolocation
+ *
+ *
+ */
+
 function geolocation() {
 
     navigator.geolocation.getCurrentPosition(onSuccess, onError);
 
-
-    // onSuccess Geolocation
-    //
     function onSuccess(position) {
         window.localStorage.setItem("lat", position.coords.latitude);
         window.localStorage.setItem("lon", position.coords.longitude);
-        //alert (lat + "<br>" + window.localStorage.getItem("lat"));
     }
 
     function onError(error) {
