b35d2a184263aa2479a50488d609090d26f6a95b
quxx <Thomas.Claudi@iem.thm.de>
2015-09-30 16:13:15 +0200

    ajaxRequest

--- www/scripts/ajaxRequest.js
+++ www/scripts/ajaxRequest.js
@@ -6,99 +6,52 @@
  @param sContent (optional) POST-Content passed to the server
  */
 function AjaxRequest(sURL, fCallback, sContent)
-
 {
-
     this.url = sURL;
-
-    if(AjaxRequest.arguments.length < 1)
-
-        this.url = "";
-
+    if (AjaxRequest.arguments.length < 1) this.url = "";
     this.callback = fCallback;
-
-    if(AjaxRequest.arguments.length < 2)
-
-        this.callback = null;
-
+    if (AjaxRequest.arguments.length < 2) this.callback = null;
     this.content = sContent;
-
-    if(AjaxRequest.arguments.length < 3)
-
-        this.content = "";
-
+    if (AjaxRequest.arguments.length < 3) this.content = "";
     this.httpReq = null;
-
     this.send = function()
-
     {
-
-        if(this.url.length <= 0)
-
-            return false;
-
+        if (this.url.length <= 0) return false;
         if (window.XMLHttpRequest && (window.location.protocol !== "file:" || !window.ActiveXObject))
-
         {
-
             this.httpReq = new XMLHttpRequest();
-
         }
-
         else
-
         {
             try
             {
-
                 this.httpReq = new ActiveXObject("Microsoft.XMLHTTP");
             }
-            catch(e)
+            catch (e)
             {
-                this.httpReq = null;            
+                this.httpReq = null;
             }
-
         }
-
         if (this.httpReq != null)
-
         {
-
             var method = (this.content.length > 0) ? "POST" : "GET";
-
             this.httpReq.open(method, this.url, true);
-
-            if(this.content.length > 0)
-
-                this.httpReq.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-
+            if (this.content.length > 0) this.httpReq.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
             var tmp = this;
-
-            this.httpReq.onreadystatechange = function(){tmp.onRequestStateChange();};
-
+            this.httpReq.onreadystatechange = function()
+            {
+                tmp.onRequestStateChange();
+            };
             this.httpReq.send(this.content);
             return true;
-
         }
-        else
-            return false;
-
+        else return false;
     }
-
     this.onRequestStateChange = function()
-
     {
-
-        if(this.httpReq.readyState == 4)
-
+        if (this.httpReq.readyState == 4)
         {
-
-            if(this.callback != null)
-
-                this.callback(this.httpReq.responseText);
-
+            if (this.callback != null) this.callback(this.httpReq.responseText);
         }
-
     }
-
-}  
+}
