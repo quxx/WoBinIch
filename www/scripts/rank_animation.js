39aa7d3bd12292f33ea0a741112d37572e757a48
quxx <Thomas.Claudi@iem.thm.de>
2015-09-30 15:56:45 +0200

    vielleicht?

--- www/scripts/rank_animation.js
+++ www/scripts/rank_animation.js
@@ -14,140 +14,167 @@
     ret.push(points);
     return ret;
 }
-var viewport = {
-    width: $(window)
-        .width(),
-    height: $(window)
-        .height()
-};
-var ct = {
-        canvas: null,
-        ctx: null,
-        values: [],
-        newValues: [],
-        barWidth: 40,
-        barFill: null,
-        backgroundFill: null,
-        scale: 0,
-        duration: 2.0,
-        fps: 30,
-        startTime: 0,
-        timer: null,
-        maxP: 0,
-        init: function()
-        {
-            var ret = scoreArray();
-            ct.values = ret[1];
-            ct.canvas = document.getElementById("myCanvas");
-            ct.canvas.width = viewport.width - 5;
-            ct.canvas.height = viewport.height - 50;
-            if (ct.canvas && ct.canvas.getContext)
-            {
-                ct.ctx = ct.canvas.getContext("2d");
-                //Farbverläufe
-                ct.barFill = ct.ctx.createLinearGradient(0, 0, 0, 350);
-                ct.barFill.addColorStop(0.0, "#F5A9A9");
-                ct.barFill.addColorStop(0.5, "#FE2E2E");
-                ct.barFill.addColorStop(1.0, "#DF0101");
-                ct.backgroundFill = "#F2F2F2";
-                //Array aufsteigen sortieren
-                ct.values.sort(function(a, b)
-                {
-                    return b - a
-                });
-                //Punkte in relation zum höchsten Punktestand setzen
-                ct.maxP = ct.values[0];
-                for (var i = 0; i < ct.values.length; i++)
-                {
-                    ct.newValues[i] = ((ct.values[i] * (ct.canvas.height - 50)) / ct.maxP);
-                }
-            }
-            ct.animStart();
-        },
-        animStart: function()
-        {
-            ct.startTime = new Date()
-                .getTime();
-            ct.timer = setInterval(ct.animate, 1000 / ct.fps);
-        },
-        animate: function()
-        {
-            var diffTime = new Date()
-                .getTime() - ct.startTime;
-            //Skallierungsfaktor
-            ct.scale = diffTime / (1000 * ct.duration);
-            //Ende
-            if (diffTime >= 1000 * ct.duration)
-            {
-                ct.scale = 1.0; //auf 1.0 setzen damit Säule am Ende den richtigen Wert/Größe hat
-                clearInterval(ct.timer);
-            }
-            ct.draw();
-        },
-        draw: function()
-        {
-            //Hintergrund zeichnen
-            ct.ctx.fillStyle = ct.backgroundFill;
-            ct.ctx.fillRect(0, 0, ct.canvas.width, ct.canvas.height);
-            ct.ctx.save();
-            ct.ctx.translate(20, ct.canvas.height - 30);
-            ct.ctx.scale(1, -1);
-            //Säulen zeichnen
-            for (var i = 0; i < ct.newValues.length; i++)
-            {
-                ct.ctx.fillStyle = ct.barFill;
-                ct.ctx.fillRect(i * (ct.barWidth + 20), 0, ct.barWidth, ct.scale * ct.newValues[i]);
-            }
-            ct.ctx.restore();
-        }
-    }
-    //Tabelle
-
-function generateTable()
-{
-    //Build an array containing Customer records.
+     // chart sample data
     var ret = scoreArray();
-    var names = ret[0];
-    var points = ret[1];
-    var customers = new Array();
-    customers.push(["Spieler", "Name", "Punkte"]);
-    var x;
-    for (x = 0; x < names.length-1; x++)
-    {
-        customers.push([parseInt(x + 1), names[x], points[x]]);
+    var arrVisitors = new Array();
+    for (var i=0; i <= ret[0].length-1;i++) {
+        arrVisitors[i] = ret[0][i] + "," + ret[1][i];
     }
-    //Tabellen Element erstellen
-    var table = document.createElement("TABLE");
-    table.border = "1";
-    //Spalten Anzahl
-    var columnCount = customers[0].length;
-    //Hinzufügen der Kopfzeilen
-    var row = table.insertRow(-1);
-    for (var i = 0; i < columnCount; i++)
+
+    var canvas;
+    var context;
+     // chart properties
+    var cWidth, cHeight, cMargin, cSpace;
+    var cMarginSpace, cMarginHeight;
+     // bar properties
+    var bWidth, bMargin, totalBars, maxDataValue;
+    var bWidthMargin;
+     // bar animation
+    var ctr, numctr, speed;
+     // axis property
+    var totLabelsOnYAxis;
+    document.addEventListener("pageinit", function(e)
     {
-        var headerCell = document.createElement("TH");
-        headerCell.innerHTML = customers[0][i];
-        row.appendChild(headerCell);
-    }
-    //Daten einfügen
-    for (var i = 1; i < customers.length; i++)
-    {
-        row = table.insertRow(-1);
-        for (var j = 0; j < columnCount; j++)
+        if (e.target.id == "my-ranking")
         {
-            var cell = row.insertCell(-1);
-            cell.innerHTML = customers[i][j];
+            var viewport = {
+                width: $(window)
+                    .width(),
+                height: $(window)
+                    .height()
+            };
+            //        alert(viewport.height);
+            //        alert(viewport.width);
+            canvas = document.getElementById("myCanvas");
+            canvas.width = viewport.width - 5;
+            canvas.height = viewport.height - 50;
+            if (canvas && canvas.getContext)
+            {
+                context = canvas.getContext('2d');
+            }
+            chartSettings();
+            drawAxisLabelMarkers();
+            drawChartWithAnimation();
+        }
+    });
+     // initialize the chart and bar values
+    function chartSettings()
+    {
+        // chart properties
+        cMargin = 25;
+        cSpace = 30;
+        cHeight = canvas.height - 2 * cMargin - cSpace;
+        cWidth = canvas.width - 2 * cMargin - cSpace;
+        cMarginSpace = cMargin + cSpace;
+        cMarginHeight = cMargin + cHeight;
+        // bar properties
+        bMargin = 15;
+        totalBars = arrVisitors.length;
+        bWidth = (cWidth / totalBars) - bMargin;
+        // find maximum value to plot on chart
+        maxDataValue = 0;
+        for (var i = 0; i < totalBars; i++)
+        {
+            var arrVal = arrVisitors[i].split(",");
+            var barVal = parseInt(arrVal[1]);
+            if (parseInt(barVal) > parseInt(maxDataValue)) maxDataValue = barVal;
+        }
+        totLabelsOnYAxis = 10;
+        context.font = "10pt Garamond";
+        // initialize Animation variables
+        ctr = 0;
+        numctr = 100;
+        speed = 10;
+    }
+     // draw chart axis, labels and markers
+    function drawAxisLabelMarkers()
+    {
+        context.lineWidth = "2.0";
+        // draw y axis
+        drawAxis(cMarginSpace, cMarginHeight, cMarginSpace, cMargin);
+        // draw x axis
+        drawAxis(cMarginSpace, cMarginHeight, cMarginSpace + cWidth, cMarginHeight);
+        context.lineWidth = "1.0";
+        drawMarkers();
+    }
+     // draw X and Y axis
+    function drawAxis(x, y, X, Y)
+    {
+        context.beginPath();
+        context.moveTo(x, y);
+        context.lineTo(X, Y);
+        context.closePath();
+        context.stroke();
+    }
+     // draw chart markers on X and Y Axis
+    function drawMarkers()
+    {
+        var numMarkers = parseInt(maxDataValue / totLabelsOnYAxis);
+        context.textAlign = "right";
+        context.fillStyle = "#000";;
+        // Y Axis
+        for (var i = 0; i <= totLabelsOnYAxis; i++)
+        {
+            markerVal = i * numMarkers;
+            markerValHt = i * numMarkers * cHeight;
+            var xMarkers = cMarginSpace - 5;
+            var yMarkers = cMarginHeight - (markerValHt / maxDataValue);
+            context.fillText(markerVal, xMarkers, yMarkers, cSpace);
+        }
+        // X Axis
+        context.textAlign = 'center';
+        for (var i = 0; i < totalBars; i++)
+        {
+            arrval = arrVisitors[i].split(",");
+            name = arrval[0];
+            markerXPos = cMarginSpace + bMargin + (i * (bWidth + bMargin)) + (bWidth / 2);
+            markerYPos = cMarginHeight + 10;
+            context.fillText(name, markerXPos, markerYPos, bWidth);
+        }
+        context.save();
+        // Add Y Axis title
+        context.translate(cMargin - 10, cHeight / 2);
+        context.rotate(Math.PI * -90 / 180);
+        context.fillText('Punkte', 0, 0);
+        context.restore();
+        // Add X Axis Title
+        context.fillText('Spieler', cMarginSpace + (cWidth / 2), cMarginHeight + 30);
+    }
+    
+    function drawChartWithAnimation()
+    {
+        // Loop through the total bars and draw
+        for (var i = 0; i < totalBars; i++)
+        {
+            var arrVal = arrVisitors[i].split(",");
+            bVal = parseInt(arrVal[1]);
+            bHt = (bVal * cHeight / maxDataValue) / numctr * ctr;
+            bX = cMarginSpace + (i * (bWidth + bMargin)) + bMargin;
+            bY = cMarginHeight - bHt - 2;
+            drawRectangle(bX, bY, bWidth, bHt, true);
+        }
+        // timeout runs and checks if bars have reached
+        // the desired height; if not, keep growing
+        if (ctr < numctr)
+        {
+            ctr = ctr + 1;
+            setTimeout(arguments.callee, speed);
         }
     }
-    var dvTable = document.getElementById("dvTable");
-    dvTable.innerHTML = "";
-    dvTable.appendChild(table);
-}
-document.addEventListener("pageinit", function(e)
-{
-    if (e.target.id == "my-ranking")
+    
+    function drawRectangle(x, y, w, h, fill)
     {
-        ct.init();
-        generateTable();
+        context.beginPath();
+        context.rect(x, y, w, h);
+        context.closePath();
+        context.stroke();
+        if (fill)
+        {
+            var gradient = context.createLinearGradient(0, 0, 0, 300);
+            gradient.addColorStop(0, 'rgb(235,72,47)');
+            gradient.addColorStop(1, 'rgb(242,130,39)');
+            context.fillStyle = gradient;
+            context.strokeStyle = gradient;
+            context.fill();
+        }
     }
-});
